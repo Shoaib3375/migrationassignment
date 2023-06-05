f9013a7fb6a692dbaa4eeed8af339b0a15f947b9
Md Shoaib Islam <mestabd60@gmail.com>
2023-06-05 20:20:13 +0600

    updated

--- database/migrations/2023_06_04_094623_add_category_to_products_table.php
+++ database/migrations/2023_06_04_094623_add_category_to_products_table.php
@@ -4,13 +4,11 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
-    public function up(): void
-    {
+    public function up(): void{
         Schema::table('products', function (Blueprint $table) {
             $table->after('quantity', function (Blueprint $table) {
                 $table->string('category', 50);
@@ -21,8 +19,7 @@ return new class extends Migration
     /**
      * Reverse the migrations.
      */
-    public function down(): void
-    {
+    public function down(): void{
         Schema::table('products', function (Blueprint $table) {
             $table->dropColumn('category');
         });
